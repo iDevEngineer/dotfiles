3d1b28745f96e1bfee0b9a364b5a1b107ee41aea
Scott Mackey <me@dev.engineer>
2024-02-01 22:16:46 +0000

    created color config for wezterm terminal

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,55 +6,40 @@ local act = wezterm.action
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
-return {
-	font = wezterm.font("VictorMono Nerd Font", { weight = "Regular" }),
-	font_size = 17.0,
-	line_height = 1.2,
-	color_scheme = "scottmackey",
-	default_cursor_style = "BlinkingBlock",
-	cursor_thickness = 14.0,
-	-- cursor_blink_rate = 800,
-	hide_mouse_cursor_when_typing = true,
-	enable_scroll_bar = false,
-	window_decorations = "RESIZE",
-	window_frame = {
-		font_size = 14.0,
-	},
-	window_padding = {
-		left = 10,
-		right = 10,
-		top = 10,
-		bottom = 0,
-	},
-	-- window_background_image = "/Users/scott/Library/Mobile Documents/com~apple~CloudDocs/Wallpaper/apple-wonderlust-2880x1800.jpg",
-	window_background_image_hsb = {
-		hue = 1.0,
-		saturation = 1.0,
-		brightness = 0.3,
-	},
-	window_background_opacity = 0.95,
-	hide_tab_bar_if_only_one_tab = true,
-	use_fancy_tab_bar = false,
-	tab_bar_at_bottom = true,
-	-- keys = {
-	-- 	{ key = "l", mods = "CTRL", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-	-- 	{ key = "h", mods = "CTRL", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-	-- 	{ key = "w", mods = "CTRL", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-	-- 	{ key = "t", mods = "CTRL", action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }) },
-	-- 	{ key = "w", mods = "CTRL|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-	-- 	{ key = "LeftArrow", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Left" }) },
-	-- 	{ key = "RightArrow", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Right" }) },
-	-- 	{ key = "UpArrow", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Up" }) },
-	-- 	{ key = "DownArrow", mods = "CTRL", action = wezterm.action({ ActivatePaneDirection = "Down" }) },
-	-- 	{ key = "LeftArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Left", 5 } }) },
-	-- 	{ key = "RightArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Right", 5 } }) },
-	-- 	{ key = "UpArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Up", 5 } }) },
-	-- 	{ key = "DownArrow", mods = "CTRL|SHIFT", action = wezterm.action({ AdjustPaneSize = { "Down", 5 } }) },
-	-- 	{ key = "LeftArrow", mods = "CTRL|ALT", action = wezterm.action({ AdjustPaneSize = { "Left", -5 } }) },
-	-- 	{ key = "RightArrow", mods = "CTRL|ALT", action = wezterm.action({ AdjustPaneSize = { "Right", -5 } }) },
-	-- 	{ key = "UpArrow", mods = "CTRL|ALT", action = wezterm.action({ AdjustPaneSize = { "Up", -5 } }) },
-	-- },
+config.font = wezterm.font('VictorMono Nerd Font', { weight = 'Regular' })
+config.font_size = 17.0
+config.line_height = 1.2
+config.default_cursor_style = 'BlinkingBlock'
+config.cursor_thickness = 14.0
+config.hide_mouse_cursor_when_typing = true
+config.enable_scroll_bar = false
+config.window_decorations = 'RESIZE'
+
+config.window_frame = {
+  font_size = 14.0,
 }
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 0,
+}
+config.window_background_image_hsb = {
+  hue = 1.0,
+  saturation = 1.0,
+  brightness = 0.3,
+}
+config.window_background_opacity = 0.95
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+
+config.window_background_image = '/Users/scott/Library/Mobile Documents/com~apple~CloudDocs/Wallpaper/apple-wonderlust-2880x1800.jpg'
+
+-- config.color_scheme_dirs = { '~/.config/wezterm/colors' }
+config.color_scheme = 'scottmackey'
+
+return config
